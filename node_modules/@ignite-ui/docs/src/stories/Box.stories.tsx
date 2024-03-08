292e4a67d0167cfd0767ed63ebefbc9942c82eee
junotomo <jun.atomo@gmail.com>
2024-03-08 19:50:52 -0300

    Initial commit

---
+++ node_modules/@ignite-ui/docs/src/stories/Box.stories.tsx
@@ -0,0 +1,25 @@
+
+import { Box, BoxProps, Text } from '@ignite-ui/react'
+import type { StoryObj, Meta } from '@storybook/react'
+
+export default {
+    title: 'Surfaces/Box',
+    component: Box,
+    args: {
+        children: (
+            <>
+                <Text>Testando elemento Box</Text>
+            </>
+        ),
+    },
+    argTypes: {
+        children: {
+          control: {
+            type: null,
+          },
+        },
+      },
+} as Meta<BoxProps>
+
+export const Primary: StoryObj<BoxProps> = {}
+
---
+++ packages/docs/src/stories/Heading.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from '@storybook/react'
+import { Heading, HeadingProps } from '@ignite-ui/react'
+
+export default {
+  title: 'Typography/Heading',
+  component: Heading,
+  args: {
+    children: 'Custom Title',    
+  },
+  argTypes: {
+    variant: {
+      options: ['sm', 'md', 'lg', '2xl', '4xl', '5xl', '6xl'],
+      control: {
+        type: 'inline-radio',
+      }
+    },
+  }
+} as Meta<HeadingProps>
+
+export const Primary: StoryObj<HeadingProps> = {}
+
+export const CustomTag: StoryObj<HeadingProps> = {
+  args: {
+    children: 'Strong text',
+    as: 'strong',
+  },
+  parameters: {
+    docs: {
+        description: {
+            sotry: 'Por padrão heading sempre será h2'
+        }
+        
+    }
+  }
+}
